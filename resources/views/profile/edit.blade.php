f26233a5f42211135bbce800223549a1769091c6
Dayananda Dowarah <dowarah.dayananda35@gmail.com>
2024-08-31 16:53:19 +0530

    edit layout

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,10 +1,8 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+@extends('layouts.admin')
+@section('title', 'Profile')
 
+@section('content')
+    <h2 class="text-2xl font-semibold text-gray-800 mb-4">Profile</h2>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
@@ -26,4 +24,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection
