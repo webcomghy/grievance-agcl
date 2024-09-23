a3396dc78a216a0eaff1e40ac44ae14037eccef8
Dayananda Dowarah <dowarah.dayananda35@gmail.com>
2024-09-23 17:26:55 +0530

    comment removed unused

--- resources/views/meter_uploads/set_month_and_date.blade.php
+++ resources/views/meter_uploads/set_month_and_date.blade.php
@@ -3,10 +3,10 @@
 @section('content')
     <h2 class="text-3xl font-bold text-gray-800">Dates</h2>
     <div class="flex space-x-4">
-        <div class="flex-1 p-4 bg-white shadow rounded-lg"> <!-- Changed width to 1/4 for the form -->
+        <div class="flex-1 p-4 bg-white shadow rounded-lg">
             @include('meter_uploads.partials.set_month_and_date_form')
         </div>
-        <div class="flex-1 p-4 bg-white shadow rounded-lg"> <!-- Changed width to 3/4 for the table -->
+        <div class="flex-1 p-4 bg-white shadow rounded-lg">
             @include('meter_uploads.partials.dates_table')
         </div> 
     </div>
@@ -21,9 +21,9 @@
                 ajax: "{{ route('meter_uploads.set_dates') }}",
                 pageLength: 5,
                 columns: [
-                    {data: 'month', name: 'month', render: function(data) { // 
+                    {data: 'month', name: 'month', render: function(data) { 
                         const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-                        return months[data - 1]; // Convert month number to month name
+                        return months[data - 1];
                     }},
                     {data: 'year', name: 'year'},
                     {data: 'from_date', name: 'from_date'},
