825ed3b4daa456fb1266685f38a0e39b3c628e4f
Dayananda Dowarah <dowarah.dayananda35@gmail.com>
2024-09-26 16:08:29 +0530

    refactor

--- app/Http/Controllers/GrievanceController.php
+++ app/Http/Controllers/GrievanceController.php
@@ -153,14 +153,6 @@ class GrievanceController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Grievance $grievance)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, Grievance $grievance)
@@ -201,14 +193,6 @@ class GrievanceController extends Controller
             return redirect()->route('grievances.show', $encryptedId)
                 ->with('error', 'Failed to update grievance: ');
         }
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Grievance $grievance)
-    {
-        //
     }
 
     public function outbox()
