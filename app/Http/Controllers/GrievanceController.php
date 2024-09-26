e7d088e0103ded43735631d2b36a8158b334ecde
Dayananda Dowarah <dowarah.dayananda35@gmail.com>
2024-09-26 16:06:54 +0530

    -_-

--- app/Http/Controllers/GrievanceController.php
+++ app/Http/Controllers/GrievanceController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Grievance;
-use App\Models\GrievanceTransaction;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
