7fdf0e7b18dde7e55bfd927e6db413fe78635dff
Dayananda Dowarah <dowarah.dayananda35@gmail.com>
2024-10-04 12:21:57 +0530

    change

--- routes/roles/consumer.php
+++ routes/roles/consumer.php
@@ -3,7 +3,8 @@
 use App\Http\Controllers\GrievanceController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:consumer')->group(function () {
-    Route::get('grievances/userdata', [GrievanceController::class, 'index'])->name('grievances.indexuser');
-    Route::get('grievances/userdata/{grievance}', [GrievanceController::class, 'show'])->name('grievances.showuser');
-});
+// Route::middleware('auth:consumer')->group(function () {
+   
+//     Route::get('grievances/userdata', [GrievanceController::class, 'index'])->name('grievances.indexuser');
+//     Route::get('grievances/userdata/{grievance}', [GrievanceController::class, 'show'])->name('grievances.showuser');
+// });
